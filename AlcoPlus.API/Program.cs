ccac5dc1c7ffd40135737ca675c24996c45e16e6
Anjo Tadena <tadena.anjo@gmail.com>
2023-11-05 11:09:32 +0800

    Add cors policy allow all

--- AlcoPlus.API/Program.cs
+++ AlcoPlus.API/Program.cs
@@ -7,6 +7,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", b => b.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +22,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// for now, allow any request from our API
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
