a9a2ac4a14b8d79f10136fae9a91aebe5b77b1ff
Anjo Tadena <tadena.anjo@gmail.com>
2023-11-21 17:12:24 +0800

    009: Fix scallding and Migrations (#9)

---
+++ AlcoPlus.Data/AlcoPlusDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace AlcoPlus.Data;
+
+public class AlcoPlusDbContextFactory : IDesignTimeDbContextFactory<AlcoPlusDbContext>
+{
+    public AlcoPlusDbContext CreateDbContext(string[] args)
+    {
+        IConfiguration configuration = new ConfigurationBuilder()
+                                            .SetBasePath(Directory.GetCurrentDirectory())
+                                            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                                            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<AlcoPlusDbContext>();
+        var connectionString = configuration.GetConnectionString("AlcoPlusApiDbConnectionString");
+
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new AlcoPlusDbContext(optionsBuilder.Options);
+    }
+}
